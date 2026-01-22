354e62f18fa14fe1365b6027c5c228ea26ce1d85
RubizZ <ruben200409@gmail.com>
2026-01-22 16:45:25 +0100

    Add AppError construction

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,10 @@
+import 'dotenv/config'
 import 'reflect-metadata'
 import express from 'express';
 import { connectDB } from './config/database.js';
 import { RegisterRoutes } from '../build/routes.js';
 import { ValidateError } from 'tsoa';
+import { AppError } from './utils/errors.js';
 
 const PORT = process.env.PORT || 3000;
 
@@ -17,12 +19,21 @@ RegisterRoutes(app)
 
 app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction): express.Response | void => {
     if (err instanceof ValidateError) {
-        console.warn(`Unhandled Error on path ${req.path}:\n`, err);
+        console.log(`ValidateError on path ${req.path}:\n`, err);
         return res.status(422).json({
             status: 'fail',
             data: {
                 message: 'Validation Failed',
                 details: err.fields,
+            },
+        });
+    }
+    if (err instanceof AppError) {
+        console.log(`AppError on path ${req.path}:\n`, err);
+        return res.status(err.statusCode).json({
+            status: 'fail',
+            data: {
+                message: err.message,
             },
         });
     }
