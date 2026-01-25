4339537e95bb5de490b0008d7d157b3c74becc6c
Leonardo <leonardo.ulffe.2003@gmail.com>
2026-01-25 18:49:18 +0100

    implement airline search enpoint

--- backend/src/modules/flights/models/airline.model.ts
+++ backend/src/modules/flights/models/airline.model.ts
@@ -2,14 +2,16 @@ import { Schema, model } from "mongoose";
 
 export interface IAirline {
   name: string;
+  iata_code?: string;
+  icao_code?: string;
   country: string;
-  quality_score: number;
 }
 
 const AirlineSchema = new Schema<IAirline>({
   name: { type: String, required: true },
+  iata_code: { type: String, index: true },
+  icao_code: { type: String, index: true },
   country: { type: String, required: true },
-  quality_score: { type: Number, required: true }
 });
 
 export const Airline = model<IAirline>("Airline", AirlineSchema);
